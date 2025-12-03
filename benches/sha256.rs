e13599f95a834ef89207989e7d08bf80b33604ed
enzoblain <blenzodu57@gmail.com>
2025-12-03 22:50:17 +0100

    ci: bench and test

--- benches/sha256.rs
+++ benches/sha256.rs
@@ -4,9 +4,7 @@ use criterion::{Criterion, criterion_group, criterion_main};
 use std::hint::black_box;
 
 pub fn bench_sha256(c: &mut Criterion) {
-    c.bench_function("sha256 64 bytes", |b| {
-        b.iter(|| sha256(black_box(&[0u8; 64])))
-    });
+    c.bench_function("sha256", |b| b.iter(|| sha256(black_box(&[0u8; 64]))));
 }
 
 criterion_group!(benches, bench_sha256);
--- benches/sha256_ref.rs
+++ benches/sha256_ref.rs
@@ -2,17 +2,15 @@ use criterion::{Criterion, criterion_group, criterion_main};
 use sha2::{Digest, Sha256};
 use std::hint::black_box;
 
-pub fn bench_sha2_crate(c: &mut Criterion) {
-    let data = [0u8; 64];
-
-    c.bench_function("sha2::Sha256 64 bytes", |b| {
+pub fn bench_sha2_ref(c: &mut Criterion) {
+    c.bench_function("sha2", |b| {
         b.iter(|| {
             let mut hasher = Sha256::new();
-            hasher.update(black_box(&data));
+            hasher.update(black_box(&[0u8; 64]));
             let _ = hasher.finalize();
         })
     });
 }
 
-criterion_group!(benches, bench_sha2_crate);
+criterion_group!(benches, bench_sha2_ref);
 criterion_main!(benches);
